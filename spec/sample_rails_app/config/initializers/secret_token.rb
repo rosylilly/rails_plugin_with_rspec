70bbcffce3692393f27b850a944f242ba863f93c
Sho Kusano <rosylilly@aduca.org>
2012-04-22 04:34:11 +0900

    $ mkdir spec && cd spec && rails new sample_rails_app -T

---
+++ spec/sample_rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsApp::Application.config.secret_token = 'e4af348b76cae77bb277ff929b01eb8adadd3bfb0e2e706e4a6a361b7b0ddf2328ab7f4c3e254258a3bd7abee936b99d6e29885d1f805ad821fa60cdc944fec9'
